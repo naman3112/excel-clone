b5f4a1054c80dfc6627ebf695d7c2ce0780d85aa
Naman <namanarora311@gmail.com>
2021-08-04 18:55:07 +0530

    excel clone row completed

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let str = "";
-let n = 27;
-while (n > 0) {
-  let rem = n % 26;
-  if(rem==0){
-      str="Z";
-      n=Math.floor(n/26)-1;
+for (let i = 1; i <= 100; i++) {
+  let str = "";
+  let n = i;
+  while (n > 0) {
+    let rem = n % 26;
+    if (rem == 0) {
+      str = "Z";
+      n = Math.floor(n / 26) - 1;
+    } else {
+      str = String.fromCharCode(rem - 1 + 65) + str;
+      n = Math.floor(n / 26);
+    }
   }
-  else{
-str=String.fromCharCode((rem-1)+65)+str;
-n=Math.floor(n/26);
-
-  }
-
+  $("#columns").append(`<div class="column-name">${str}</div>`)
 }
+
 console.log(str);
